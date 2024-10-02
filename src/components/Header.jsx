dc8a0de817469b7179cded66bca108b9d63502d0
choi_hyeona <hac0309@naver.com>
2024-10-02 21:50:19 +0900

    241002 헤더 틀 완성

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,62 @@
 import { Button, Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import styled from "styled-components";
+import { LuUser2 } from "react-icons/lu";
 
 
+const HeaderContainer = styled.div`
+  width: 100%;
+  height: 156px;
+  background-color: #fff;
+`;
 
+const HeaderInner = styled.div`
+  width: 100%;
+  max-width: 1440px;
+  background-color: #ffffff;
+  margin: 0 auto;
+  font-size: 18px;
+`;
+
+const LogoImg = styled.img`
+  width: 100px;
+`;
+
+const UserIcon = styled(LuUser2)`
+  width: 30px;
+  height: 30px;
+`;
 
 function Header() {
   return (
-    <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    <HeaderContainer>
+      <HeaderInner>
+
+        <Navbar bg="white" data-bs-theme="light">
+          <Container>
+            <Navbar.Brand href="/">
+              <LogoImg src="/images/meongday.png" alt="meongday" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="#features">게시판1</Nav.Link>
+                <Nav.Link href="#1">게시판2</Nav.Link>
+                <Nav.Link href="#2">게시판3</Nav.Link>
+                <Nav.Link href="#3">게시판4</Nav.Link>
+              </Nav>
+              <Nav>
+                <NavDropdown title={<UserIcon />} id="collapsible-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">로그인</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    마이페이지
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </HeaderInner>
+    </HeaderContainer>
   );
 };
 
