aa484d46035f9168acbbb3d85c30711650159051
choi_hyeona <hac0309@naver.com>
2024-10-02 20:55:56 +0900

    241002 부트스트랩 적용 완료 후후

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import { createGlobalStyle } from 'styled-components';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
