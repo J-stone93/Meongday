7b7755c07663e0fd1ce65612c46d855a8d3be409
yundahun <fkdlwldj02@naver.com>
2024-10-10 20:58:01 +0900

    2024.10.10 로그인 시작

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const LoginId = styled.placeholder`
+    position: absolute;
+    width: 20px;
+    height: 20px;
+`
+
+function Login() {
+  return (
+    <LoginId/>
+  );
+};
+
+export default Login;
